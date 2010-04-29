6461eba28b4655136b33c12d02cd507d178055de
John-John Tedro <udoprog@udoprog-laptop.(none)>
2010-04-29 14:13:10 +0200

    Fixed compaint by spiv

--- dclite/highprotocol.py
+++ dclite/highprotocol.py
@@ -5,41 +5,25 @@ class MyADCHubProtocol(ADCHubProtocol):
     """
     TODO: emit messages to hubwindow properly
     """
+    
     def __init__(self, hubwindow):
       hubwindow = hubwindow;
       ADCHubProtocol.__init__(self);
       
-      def on_hub_identified(hub):
-        hubwindow.emit("hub-identified", hub)
-
-      def on_status(status):
-        hubwindow.emit("status", status);
+      hubsignals = [
+        ("hub-identified",  "hub-identified"),
+        ("status",          "status"),
+        ("message",         "on-message"),
+        ("user-info",       "user-info"),
+        ("user-quit",       "user-quit"),
+        ("connection-made", "connectino-made"),
+        ("connection-lost", "connectino-lost"),
+      ];
       
-      def on_message(user, message):
-        hubwindow.emit("on-message", user, message);
-
-      def on_user_info(user):
-        hubwindow.emit("user-info", user);
-      
-      def on_user_quit(user):
-        hubwindow.emit("user-quit", user);
-      
-      def on_connection_made():
-        hubwindow.emit("connection-made");
-      
-      def on_connection_lost(reason):
-        hubwindow.emit("connection-lost", reason);
-      
-      self.connect("hub-identified", on_hub_identified);
-      self.connect("status", on_status);
-      self.connect("message", on_message);
-      self.connect("user-info", on_user_info);
-      self.connect("user-quit", on_user_quit);
-      self.connect("connection-made", on_connection_made);
-      self.connect("connection-lost", on_connection_lost);
+      for fr, to in hubsignals:
+        self.connect(fr, lambda *args: hubwindow.emit(to, *args));
       
       # private signals
-      self.connect("get-user", self.getUser);
       self.connect("direct-connect", self.onDirectConnect);
       
       hubwindow.connect("input-message-submit", self.on_inputSendMessage);
